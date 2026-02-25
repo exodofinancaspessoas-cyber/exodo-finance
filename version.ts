47153674ad14aeb39a5f2d2f3c23823f19d5094f
Bruno Rodrigues Assunção <exodofinancaspessoas@gmail.com>
2026-02-25 15:41:55 -0300

    Build 1.5.6

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 
-const APP_VERSION = '1.5.5'; // Increment this on every deploy
-const DEPLOY_DATE = '2026-02-25 15:40';
+const APP_VERSION = '1.5.6'; // Increment this on every deploy
+const DEPLOY_DATE = '2026-02-25 15:45';
 
 export const VersionInfo = {
     version: APP_VERSION,
